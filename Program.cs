e593893955e123c7b178ec647c3aebdc559432b7
Aobakwe Ntshekisang <71647234+Aobakwe-Ntshekisang@users.noreply.github.com>
2026-09-10 10:32:33 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -165,9 +165,3 @@ class TvSchedule
         }
     }
 }
-
-public class Show
-{
-    public string? Name { get; set; }
-    public int EpisodeLength { get; set; }
-}
---
+++ Show.cs
@@ -0,0 +1,5 @@
+public class Show
+{
+    public string? Name { get; set; }
+    public int EpisodeLength { get; set; }
+}
